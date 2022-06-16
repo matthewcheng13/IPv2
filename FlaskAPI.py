053922c6c3e272fc2bd45df0fe10e2d89175af50
matthewcheng13 <mcheng.mm@gmail.com>
2022-06-16 13:20:50 -0400

    Download csv and json now working

--- FlaskAPI.py
+++ FlaskAPI.py
@@ -1,6 +1,7 @@
 import csv
 import main
-from flask import Flask, redirect, url_for, request, jsonify, render_template
+import pandas as pd
+from flask import Flask, redirect, url_for, request, jsonify, render_template, make_response
 
 app = Flask(__name__)
 
@@ -8,16 +9,12 @@ app = Flask(__name__)
 @app.route('/success/<name>')
 def success(name):
     names = name.split(',')
+    global value
     value = main.multi_thread(names, main.threadForSingleAddress)
     kind = "Find All Info"
-    #save(name)
+    global columns
     columns = ['Input','IP Address','Hostname','Pingable','Open Ports']
     return render_template('info.html', name=names, value=value, kind=kind, iterate=range(len(names)), columns=columns)
-
-'''jsonify({'ip_address': nameData[0],
-                    'hostname': nameData[1],
-                    'pingable': nameData[2],
-                    'open_ports': nameData[3]})'''
 
 
 def save(name):
@@ -32,8 +29,10 @@ def save(name):
 @app.route('/ip_address_and_hostname/<name>')
 def address(name):
     names = name.split(',')
+    global value
     value = main.multi_thread(names, main.getNameAndAddress)
     kind = 'IP Address and Hostname Lookup'
+    global columns
     columns = ['Input', 'IP Address', 'Hostname']
     return render_template('info.html', name=names, value=value, kind=kind, iterate=range(len(names)), columns=columns)
 
@@ -41,8 +40,10 @@ def address(name):
 @app.route('/check_pingable/<name>')
 def do_ping(name):
     names = name.split(',')
+    global value
     value = main.multi_thread(names, main.pingable)
     kind = 'Check Pingability'
+    global columns
     columns = ['Input','Pingable']
     return render_template('info.html', name=names, value=value, kind=kind, iterate=range(len(names)), columns=columns)
 
@@ -50,8 +51,10 @@ def do_ping(name):
 @app.route('/check_open_ports/<name>')
 def ports(name):
     names = name.split(',')
+    global value
     value = main.multi_thread(names, main.check_ports)
     kind = 'Check Open Ports'
+    global columns
     columns = ['Input', 'Open Ports']
     return render_template('info.html', name=names, value=value, kind=kind, iterate=range(len(names)), columns=columns)
 
@@ -67,6 +70,36 @@ def index():
         return redirect(url_for('do_ping', name=user))
     else:
         return redirect(url_for('ports', name=user))
+
+
+@app.route('/receiver', methods=['POST'])
+def receiver():
+    if request.form.get('csv'):
+        return redirect(url_for('download_csv'))
+    elif request.form.get('json'):
+        return redirect(url_for('download_json'))
+
+
+def make_df():
+    return pd.DataFrame(value, columns=columns[1:])
+
+
+@app.route('/download_csv')
+def download_csv():
+    response = make_response(make_df().to_csv())
+    cd = 'attachment; filename=download.csv'
+    response.headers['Content-Disposition'] = cd
+    response.mimetype = 'text/csv'
+    return response
+
+
+@app.route('/download_json')
+def download_json():
+    response = make_response(jsonify(make_df().to_json()))
+    cd = 'attachment; filename=download.json'
+    response.headers['Content-Disposition'] = cd
+    response.mimetype = 'text/json'
+    return response
 
 
 if __name__ == '__main__':
