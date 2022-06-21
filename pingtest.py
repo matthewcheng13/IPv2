2ffc6d6aebecbf667a37594de8d7096421739050
matthewcheng13 <mcheng.mm@gmail.com>
2022-06-21 09:00:07 -0400

    Edited test file

--- pingtest.py
+++ pingtest.py
@@ -1,8 +1,13 @@
 import subprocess
 
 
-def ping(addr):
-    return subprocess.check_output("ping -c 1 " + addr, shell=True)
+def ping(address):
+    try:
+        return subprocess.check_output("ping -c 1 " + address, shell=True)
+    except subprocess.CalledProcessError:
+        return "Subprocess"
+    except:
+        return "Other"
 
 
 print(ping('7.7.7.7'))
